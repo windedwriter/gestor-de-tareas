bdb360e0700674072924b0f9e52bd24367f7db62
windedwriter <157185510+windedwriter@users.noreply.github.com>
2024-12-19 23:30:50 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const CONFIG = {
+    BASE_URL: 'https://windedwriter.github.io/gestor-de-tareas',
+    API_URL: 'https://sql311.infinityfree.com/auth.php',
+    ROUTES: {
+        LOGIN: '/login.html',
+        GESTOR: '/gestor_tareas.html',
+        INDEX: '/index.html'
+    }
+};
