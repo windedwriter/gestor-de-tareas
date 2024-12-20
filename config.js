3204af9a7e3f53c50cbb6f9259d19b068f972e4e
windedwriter <157185510+windedwriter@users.noreply.github.com>
2024-12-20 01:52:24 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const CONFIG = {
     BASE_URL: 'https://windedwriter.github.io/gestor-de-tareas',
-    API_URL: 'https://gestortareas.freesite.online/auth.php',  
+    API_URL: 'https://if0_37944823.infinityfree.com/auth.php', // Actualizar con tu subdominio
     ROUTES: {
-        LOGIN: '/index.html',
-        GESTOR: '/gestor_tareas.html',
-        REGISTRO: '/registro.html'
+        LOGIN: '/gestor-de-tareas/login.html',
+        GESTOR: '/gestor-de-tareas/gestor_tareas.html',
+        INDEX: '/gestor-de-tareas/index.html'
     }
 };
